0444203680c01a03b1e2c8fe126ada372803833d
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-02-27 12:53:08 -0400

    feat(search): make keywords optional so you can easily filter any plugin by its author or see a list of all plugins in the database

--- src/cli.rs
+++ src/cli.rs
@@ -27,7 +27,7 @@ pub fn build() -> Command<'static> {
             Command::new("search")
                 .about("Search through plugin database")
                 .arg(arg!(--author <name> "Filter by plugin author").required(false).takes_value(true))
-                .arg(arg!(<request> "Part of GitHub's author/name").multiple_values(true)),
+                .arg(arg!(<request> "Part of GitHub's author/name").required(false).multiple_values(true)),
         ])
 }
 
@@ -44,7 +44,10 @@ pub async fn handle(matches: clap::ArgMatches) -> anyhow::Result<()> {
         Some(("search", sub_matches)) => {
             let mut author = String::new();
             let should_filter_by_author = sub_matches.is_present("author");
-            let params: Vec<&str> = sub_matches.values_of("request").unwrap().collect();
+            let mut params: Vec<&str> = Vec::new();
+            if sub_matches.is_present("request") {
+                params = sub_matches.values_of("request").unwrap().collect();
+            }
             if should_filter_by_author {
                 author = sub_matches.values_of("author").unwrap().collect();
             }
