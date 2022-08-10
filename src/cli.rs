ced595b49c2b3a01514e926c263eff5553b9ff67
shift-d <shiftkeydeveloper@gmail.com>
2022-08-10 11:31:21 +0300

    [cli]: refactor; add docs
    run -> handle

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,22 @@
 use clap::{arg, command, Command};
 
+/// Structure that holds Clap's CLI state
 pub struct Application {
-    matches: clap::ArgMatches
+    matches: clap::ArgMatches,
 }
 
 impl Application {
+    /// Parse CLI arguments using Clap and provide matches
     pub fn new() -> Self {
         Self {
             matches: command!()
                 .propagate_version(true)
                 .arg_required_else_help(true)
-                .get_matches()
+                .get_matches(),
         }
     }
-    pub fn run(&self) {
+    /// Handle Clap's matches
+    pub fn handle(&self) {
         // TODO
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod cli;
 
 fn main() {
-    cli::Application::new().run()
+    cli::Application::new().handle()
 }
